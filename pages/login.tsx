5c815408eea1608d66f6ad153a1180e2e3217307
DLinh <nogamenolife.why@gmail.com>
2022-06-27 22:44:58 +0700

    feat: yup sign in form validation

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,41 @@
 /* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+interface Inputs {
+  email: string;
+  password: string;
+}
+
+const loginSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Please enter a valid email.")
+    .required("Email is required."),
+  password: yup
+    .string()
+    .required("Password is required.")
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+      "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
+    ),
+});
 
 const Login = () => {
+  const [login, setLogin] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>({ resolver: yupResolver(loginSchema) });
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {};
+
   return (
     <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
       <Head>
@@ -26,8 +58,7 @@ const Login = () => {
       />
 
       <form
-        action=""
-        method="post"
+        onSubmit={handleSubmit(onSubmit)}
         className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14"
       >
         <h1 className="text-4xl font-semibold">Sign In</h1>
@@ -35,30 +66,41 @@ const Login = () => {
           <label className="inline-block w-full">
             <input
               type="email"
-              name=""
-              id=""
+              {...register("email")}
               placeholder="Email"
               className="input"
             />
+            {errors.email && (
+              <p className="p-1 text-[13px] font-light text-orange-500">
+                {errors.email.message}
+              </p>
+            )}
           </label>
           <label className="inline-block w-full">
             <input
               type="password"
-              name=""
-              id=""
+              {...register("password")}
               placeholder="Password"
               className="input"
             />
+            {errors.password && (
+              <p className="p-1 text-[13px] font-light text-orange-500">
+                {errors.password.message}
+              </p>
+            )}
           </label>
         </div>
 
-        <button className="w-full rounded bg-[#e50914] py-3 font-semibold">
+        <button
+          onClick={() => setLogin(true)}
+          className="w-full rounded bg-[#e50914] py-3 font-semibold"
+        >
           Sign In
         </button>
 
         <div className="text-[gray]">
           New to Netflix?{" "}
-          <button type="submit" className="text-white hover:underline">
+          <button type="submit" className="text-white hover:underline" onClick={() => setLogin(false)}>
             Sign up now
           </button>
         </div>
